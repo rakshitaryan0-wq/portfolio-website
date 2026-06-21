7a0fcf1520f8232ac7111e670cc5c12a07885ced
Rakshit Aryan <rakshitaryan0@gmail.com>
2026-06-21 23:21:10 +0530

    Initial portfolio website

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+if(hamburger){
+    hamburger.addEventListener("click",()=>{
+        navLinks.classList.toggle("active");
+    });
+}
